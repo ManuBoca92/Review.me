4172d7625ef784e53d42cb899d509418b3426e9a
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-01-29 15:50:15 +0100

    Edited parent App vue file

---
+++ review.me/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+  // 209, 142, 174
+}
+</script>
+
+<style>
+html, body{
+  /* background-color: rgb(103, 185, 117); */
+  height:100%;
+  margin:0;
+  padding:0;
+}
+</style>
