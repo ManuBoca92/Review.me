bc6d1dc5bda8a36509e97a59a5a59bc066b8f53f
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-01-29 15:34:10 +0100

    added components

---
+++ review.me/src/components/Book.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div>
+      <app-home v-model="result"></app-home>
+    </div>
+    <template>
+      <div class="container">
+        <br>
+        <div class="row">
+          <div class="col-md-4 col-xs">
+            <img :src="result.volumeInfo.imageLinks.small || result.volumeInfo.imageLinks.thumbnail" alt="">
+          </div>
+
+          <div class="col-md-8 col-xs-10">
+            <div class="col">
+              <p
+               v-for="author in result.volumeInfo.authors"
+               :key="author">
+                <strong>Authors:</strong> {{ author }}</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+              <p
+               v-for="category of result.volumeInfo.categories"
+               :key="category">
+               <strong>Categories:</strong> {{ category }}</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+              <p><strong>Ratings:</strong> {{ result.volumeInfo.averageRating }}</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+              <p><strong>No. of ratings:</strong> {{ result.volumeInfo.ratingsCount}}</p>
+            </div>
+            <div class="w-100"></div>
+            <div class="col">
+              <p><strong>Summary</strong></p>
+              <p v-html="result.volumeInfo.description"></p>
+            </div>
+          </div>
+
+        </div>
+    </div>
+    </template>
+
+  </div>
+</template>
+
+<script>
+import Home from './Home.vue'
+import {mapGetters, mapActions} from 'vuex'
+
+export default {
+  components: {
+    appHome: Home
+  },
+  computed: {
+    ...mapGetters([
+      'result'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'getBook'
+    ])
+  }
+
+}
+</script>
+
+<style scoped>
+.container {
+  width: 1650px;
+  max-width: 1450px;;
+  height: 2218px;
+  margin-top: 1%;
+  padding-left: 5%;
+  padding-right: 5%;
+}
+
+p {
+  padding-left: 10px;
+}
+</style>
---
+++ review.me/src/components/Home.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="jumbotron">
+    <div class="container">
+        <div class="row">
+        <div class="col-lg-10 col-xs-6">
+          <router-link to="/" tag="a">
+            <h3>review.me</h3>
+          </router-link>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-xs-6">
+          <input
+            type="search"
+            v-model="$store.state.text"
+           >
+        </div>
+        <div class="col-xs-6">
+          <router-link to="/book">
+              <button @click="getBook">GO</button>
+            </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'text'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'getBook'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  font-family: Herculanum;
+  font-size: 60px;
+  text-align: left;
+  text-shadow: 0px 6px 10px #000000;
+  opacity: 100px;
+  width: 355px;
+  height: 70px;
+  cursor: pointer;
+  padding-left: 200px;
+}
+
+input[type=search]{
+  border-color: #000000;
+  opacity: 100px;
+  width: 672px;
+  height: 40px;
+  font-size: 15px;
+  font-family: Herculanum;
+  border-radius: 45px;
+  border: #000000 0.1px;
+  text-align: left;
+  box-shadow: 0px 2px 30px;
+  outline:none;
+  padding: 20px;
+}
+
+.center {
+  display: table;
+  width: 40%;
+  margin: 0 auto;
+}
+
+a {
+  text-decoration: none;
+}
+
+.search {
+  display: flex;
+}
+
+button {
+  background-color: #ffffff;
+  border: none;
+  color: #000000;
+  width: 100px;
+  height: 40px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
+  border-radius: 20px;
+  margin-left: 20px;
+  box-shadow: 0px 2px 30px;
+  cursor: pointer;
+}
+</style>
